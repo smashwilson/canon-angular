291de50f033cc0331b494a1789f02d6dc78c3035
Chris Burrell <chris.burrell@rackspace.com>
2013-09-30 17:36:02 -0500

    adding .jshintrc file and tooltip spec.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,40 @@ module.exports = function(grunt) {
  
   grunt.initConfig({
     jshint: {
-      all: ['Gruntfile.js']
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['app/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile'],
+        options: {
+          livereload: true
+        }
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib'],
+        options: {
+          livereload: true
+        }
+      }
     }
   });
  
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', 'jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint']);
  
 };
